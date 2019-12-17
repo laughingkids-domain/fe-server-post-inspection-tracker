f6700a0fb2746a0a4757b34d96f1036337362fd2
laughingkids-domain <thomas.wang@domain.com.au>
2019-12-18 09:36:26 +1100

    fe-pit add listing detail for user response

--- graph-server/resolvers/Query/user.js
+++ graph-server/resolvers/Query/user.js
@@ -2,13 +2,28 @@
 const fs = require("fs");
 const userDetails = fs.readFileSync(`${__dirname}/users.json`);
 const mockUsers = JSON.parse(userDetails);
+const axios = require("axios");
+const { map } = require("p-iteration");
+const token = "14885191b5b20df85525168954fe982a";
 
-function user(parent, args, context) {
+async function user(parent, args, context) {
   try {
     const user = mockUsers.find(user => user.userId === args.id);
     const avatarFileName = `${user.firstName}${user.lastName}`.toLowerCase();
     const avatarUrl = `https://api.adorable.io/avatars/285/${avatarFileName}`;
-    return { ...user, avatarUrl };
+    let ownedProperties = [];
+    if (user.ownedProperties) {
+      ownedProperties = await user.ownedProperties.map(async listing => {
+        const listingUrl = `https://api.domain.com.au/sandbox/v1/listings/${listing}`;
+        const result = await axios.get(listingUrl, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        return result.data;
+      });
+    }
+    return { ...user, avatarUrl, ownedProperties };
   } catch (err) {
     console.error(err);
   }
